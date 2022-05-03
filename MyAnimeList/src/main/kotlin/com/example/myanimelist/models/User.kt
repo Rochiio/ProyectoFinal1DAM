fed2d73b233607f4b13333284e101d7ba13b1668
JoaquinAyG <90848989+JoaquinAyG@users.noreply.github.com>
2022-05-03 10:03:34 +0200

    Models

--- MyAnimeList/src/main/kotlin/com/example/myanimelist/models/User.kt
+++ MyAnimeList/src/main/kotlin/com/example/myanimelist/models/User.kt
@@ -2,14 +2,15 @@ package com.example.myanimelist.models
 
 import java.net.URL
 import java.sql.Date
+import java.util.*
 
 data class User(
 
-    val id: Int,
+    val id: UUID = UUID.randomUUID(),
     var name: String,
     var img: URL?,
     val createDate: Date,
     var password : String,
     var myList: List<Anime>,
 
-)
+    )
