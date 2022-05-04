ae0b63f912809919d789ae1ef270ae7a8f2698d9
Kino <joaquin.ayllongar@gmail.com>
2022-05-04 21:47:15 +0200

    inited AnimeRepositoryTest.kt

---
+++ MyAnimeList/src/test/kotlin/com/example/myanimelist/repositories/AnimeRepositoryTest.kt
@@ -0,0 +1,36 @@
+package com.example.myanimelist.repositories
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class AnimeRepositoryTest {
+
+    @Test
+    fun findbyId() {
+    }
+
+    @Test
+    fun findAll() {
+    }
+
+    @Test
+    fun update() {
+    }
+
+    @Test
+    fun add() {
+    }
+
+    @Test
+    fun delete() {
+    }
+
+    @Test
+    fun findByTitle() {
+    }
+
+    @Test
+    fun getDb() {
+    }
+}
