296c6478c2d357b6d3c8f73e705e82dad1ef5506
JoaquinAyG <90848989+JoaquinAyG@users.noreply.github.com>
2022-05-05 13:34:38 +0200

    commented repository

--- MyAnimeList/src/main/kotlin/com/example/myanimelist/repositories/animes/AnimeRepository.kt
+++ MyAnimeList/src/main/kotlin/com/example/myanimelist/repositories/animes/AnimeRepository.kt
@@ -8,9 +8,21 @@ import com.example.myanimelist.models.Type
 import java.sql.SQLException
 import java.util.*
 
+/**
+ * @author JoaquinAyG
+ * Repository to manage the anime in the database
+ */
 object AnimeRepository : IAnimeRepository {
 
+    /**
+     * Init the data base instance to use
+     */
     val db: DataBaseManager = DataBaseManager.getInstance()
+
+    /**
+     * @param id responds to the uuid assigned in the database for the object
+     * @return the aime with the id if located, null if not
+     */
     override fun findById(id: UUID): Anime? {
         val query = "SELECT * FROM animes WHERE id = ?"
         try {
@@ -42,6 +54,9 @@ object AnimeRepository : IAnimeRepository {
         return null
     }
 
+    /**
+     * @return a list with all the anime in the table, the list is empty if no anime was found
+     */
     override fun findAll(): List<Anime> {
         val query = "SELECT * FROM animes"
         val animes = ArrayList<Anime>()
@@ -72,6 +87,10 @@ object AnimeRepository : IAnimeRepository {
         return animes
     }
 
+    /**
+     * @param item Is the object Anime to update
+     * @return the item after the changes
+     */
     override fun update(item: Anime): Anime {
         val query = "UPDATE animes SET " +
                 "title = ?," +
@@ -107,6 +126,10 @@ object AnimeRepository : IAnimeRepository {
         return item
     }
 
+    /**
+     * @param item Is the object to add into the table
+     * @return The item added to the table
+     */
     override fun add(item: Anime): Anime {
         val query = "INSERT INTO animes VALUES( ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )"
         try {
@@ -132,6 +155,9 @@ object AnimeRepository : IAnimeRepository {
         return item
     }
 
+    /**
+     * @param id the id of the anime to delete in the table
+     */
     override fun delete(id: UUID) {
         val query = "DELETE FROM animes WHERE id = ?"
         try {
@@ -144,6 +170,10 @@ object AnimeRepository : IAnimeRepository {
         }
     }
 
+    /**
+     * @param title The title to search on the table
+     * @return the anime with the indicated title or null if not found
+     */
     override fun findByTitle(title: String): Anime? {
         val query = "SELECT * FROM animes WHERE title = ? OR title_english = ?"
         try {
