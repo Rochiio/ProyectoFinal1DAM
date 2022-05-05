42cfe6778d69386ce046155d29776337b1b15aba
JoaquinAyG <90848989+JoaquinAyG@users.noreply.github.com>
2022-05-05 13:24:54 +0200

    Feature anime repository (#39)
    * inited AnimeRepositoryTest.kt
    
    * Anime repository Test
    
    * ICRUDRepository.kt cleanup
    
    * anime repository fix
    
    * anime repository fix
    
    * anime repository fix
    
    * anime repository fix and Test done
    
    * add try catch to repository
    
    * add try catch to repository

--- MyAnimeList/src/main/kotlin/com/example/myanimelist/repositories/animes/AnimeRepository.kt
+++ MyAnimeList/src/main/kotlin/com/example/myanimelist/repositories/animes/AnimeRepository.kt
@@ -1,20 +1,25 @@
 package com.example.myanimelist.repositories.animes
 
 import com.example.myanimelist.managers.DataBaseManager
-import com.example.myanimelist.models.*
-import java.sql.Date
+import com.example.myanimelist.models.Anime
+import com.example.myanimelist.models.Genre
+import com.example.myanimelist.models.Status
+import com.example.myanimelist.models.Type
 import java.sql.SQLException
 import java.util.*
-import kotlin.collections.ArrayList
 
 object AnimeRepository : IAnimeRepository {
 
-    val db : DataBaseManager = DataBaseManager.getInstance()
+    val db: DataBaseManager = DataBaseManager.getInstance()
     override fun findById(id: UUID): Anime? {
         val query = "SELECT * FROM animes WHERE id = ?"
-        db.open()
-        val result = db.select(query, id.toString()).get()
-        if (result.next()) {
+        try {
+            db.open()
+            val result = db.select(query, id.toString()).get()
+
+            if (result.next()) return null
+
+
             val anime = Anime.AnimeBuilder(
                 id = UUID.fromString(result.getString("id")),
                 title = result.getString("title"),
@@ -29,33 +34,42 @@ object AnimeRepository : IAnimeRepository {
             ).build()
             db.close()
             return anime
+
+        } catch (e: SQLException) {
+            e.printStackTrace()
+        } finally {
+            db.close()
         }
-        db.close()
         return null
     }
 
     override fun findAll(): List<Anime> {
         val query = "SELECT * FROM animes"
-        db.open()
-        val result = db.select(query).get()
         val animes = ArrayList<Anime>()
-        while (result.next()) {
-            animes.add(
-                Anime(
-                    id = UUID.fromString(result.getString("id")),
-                    title = result.getString("title"),
-                    titleEnglish = result.getString("title_english"),
-                    types = Type.valueOf(result.getString("type")),
-                    episodes = result.getInt("episodes"),
-                    status = Status.valueOf(result.getString("status")),
-                    date = result.getDate("releaseDate"),
-                    rating = result.getString("rating"),
-                    genres = result.getString("genre").split(",").map { Genre.valueOf(it) },
-                    img = result.getString("imageUrl")
+        try {
+            db.open()
+            val result = db.select(query).get()
+            while (result.next()) {
+                animes.add(
+                    Anime(
+                        id = UUID.fromString(result.getString("id")),
+                        title = result.getString("title"),
+                        titleEnglish = result.getString("title_english"),
+                        types = Type.valueOf(result.getString("type")),
+                        episodes = result.getInt("episodes"),
+                        status = Status.valueOf(result.getString("status")),
+                        date = result.getDate("releaseDate"),
+                        rating = result.getString("rating"),
+                        genres = result.getString("genre").split(",").map { Genre.valueOf(it) },
+                        img = result.getString("imageUrl")
+                    )
                 )
-            )
+            }
+        } catch (e: SQLException) {
+            e.printStackTrace()
+        } finally {
+            db.close()
         }
-        db.close()
         return animes
     }
 
@@ -71,33 +85,75 @@ object AnimeRepository : IAnimeRepository {
                 "rating = ?," +
                 "type = ?" +
                 "WHERE id = ?"
-        db.open()
-        db.update(query, item.title, item.titleEnglish, item.status.toString(), item.genres.joinToString(separator = ", "), item.date, item.img, item.episodes, item.rating, item.types.toString(), item.id.toString() )
-        db.close()
+        try {
+            db.open()
+            db.update(
+                query,
+                item.title,
+                item.titleEnglish,
+                item.status.toString(),
+                item.genres.joinToString(separator = ", "),
+                item.date,
+                item.img,
+                item.episodes,
+                item.rating,
+                item.types.toString(),
+                item.id.toString()
+            )
+        } catch (e: SQLException) {
+            e.printStackTrace()
+        } finally {
+            db.close()
+        }
         return item
     }
 
     override fun add(item: Anime): Anime {
         val query = "INSERT INTO animes VALUES( ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )"
-        db.open()
-        db.insert(query, item.id, item.title, item.titleEnglish, item.status, item.genres.joinToString(separator = ", "), item.date, item.img, item.episodes, item.rating, item.types )
-        db.close()
+        try {
+            db.open()
+            db.insert(
+                query,
+                item.id,
+                item.title,
+                item.titleEnglish,
+                item.status,
+                item.genres.joinToString(separator = ", "),
+                item.date,
+                item.img,
+                item.episodes,
+                item.rating,
+                item.types
+            )
+        } catch (e: SQLException) {
+            e.printStackTrace()
+        } finally {
+            db.close()
+        }
         return item
     }
 
     override fun delete(id: UUID) {
         val query = "DELETE FROM animes WHERE id = ?"
-        db.open()
-        db.delete(query, id)
-        db.close()
+        try {
+            db.open()
+            db.delete(query, id)
+        } catch (e: SQLException) {
+            e.printStackTrace()
+        } finally {
+            db.close()
+        }
     }
 
     override fun findByTitle(title: String): Anime? {
         val query = "SELECT * FROM animes WHERE title = ? OR title_english = ?"
-        db.open()
-        val result = db.select(query, title, title).get()
-        if (result.next()) {
-            val anime = Anime.AnimeBuilder(
+        try {
+            db.open()
+            val result = db.select(query, title, title).get()
+
+            if (!result.next()) return null
+
+            val anime = Anime(
                 id = UUID.fromString(result.getString("id")),
                 title = result.getString("title"),
                 titleEnglish = result.getString("title_english"),
@@ -108,11 +164,14 @@ object AnimeRepository : IAnimeRepository {
                 rating = result.getString("rating"),
                 genres = result.getString("genre").split(",").map { Genre.valueOf(it) },
                 img = result.getString("imageUrl")
-            ).build()
+            )
             db.close()
             return anime
+        } catch (e: SQLException) {
+            e.printStackTrace()
+        } finally {
+            db.close()
         }
-        db.close()
         return null
     }
 }
