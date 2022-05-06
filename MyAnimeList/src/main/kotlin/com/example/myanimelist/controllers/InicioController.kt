e24ab73d73898cfe765039752852721db57783f7
Rocio <rpalaofernandez@gmail.com>
2022-05-06 13:55:10 +0200

    css registro add, splash de nuevo

--- MyAnimeList/src/main/kotlin/com/example/myanimelist/controllers/InicioController.kt
+++ MyAnimeList/src/main/kotlin/com/example/myanimelist/controllers/InicioController.kt
@@ -1,4 +1,26 @@
 package com.example.myanimelist.controllers
 
+import javafx.event.ActionEvent
+import javafx.fxml.FXML
+import javafx.scene.control.PasswordField
+import javafx.scene.control.TextField
+
 class InicioController {
+    @FXML
+    lateinit var txtUsername: TextField
+    @FXML
+    lateinit var txtPassword: PasswordField
+    @FXML
+    lateinit var txtUsernameRegister: TextField
+    @FXML
+    lateinit var txtEmail: TextField
+    @FXML
+    lateinit var txtPasswordRegister: PasswordField
+    @FXML
+    lateinit var txtConfirmPassword: PasswordField
+
+
+    fun changeSceneRegister(actionEvent: ActionEvent) {
+
+    }
 }
