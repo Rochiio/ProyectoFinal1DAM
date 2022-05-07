1e0df78bd0d2a1256ce1457e6b8530704a848417
Kino <joaquin.ayllongar@gmail.com>
2022-05-07 10:30:21 +0200

    Testing backup

---
+++ MyAnimeList/src/test/kotlin/com/example/myanimelist/services/AnimeStorageTest.java
@@ -0,0 +1,22 @@
+package com.example.myanimelist.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnimeStorageTest {
+
+    @Test
+    void save() {
+        assertAll(
+                () -> assertTrue(true)
+        );
+    }
+
+    @Test
+    void load() {
+        assertAll(
+                () -> assertTrue(true)
+        );
+    }
+}
