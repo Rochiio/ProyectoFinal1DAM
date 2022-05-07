b95e3d6ec5734d980a40e224d58f9d6cf5bf1a86
JoaquinAyG <90848989+JoaquinAyG@users.noreply.github.com>
2022-05-07 11:06:24 +0200

    Feature backup (#46)
    * Start backup and dto
    
    * Testing backup
    
    * add todo to test

---
+++ MyAnimeList/src/main/java/com/example/myanimelist/dto/AnimeDTO.java
@@ -0,0 +1,54 @@
+package com.example.myanimelist.dto;
+
+import com.example.myanimelist.models.Anime;
+import lombok.Data;
+
+import java.sql.Date;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Data
+public class AnimeDTO {
+    UUID id;
+    String title;
+    String titleEnglish;
+    String types;
+    Integer episodes;
+    String status;
+    String date;
+    String rating;
+    String genres;
+    String img = null;
+
+    public AnimeDTO(Anime anime){
+        this.id = anime.getId();
+        this.title = anime.getTitle();
+        this.titleEnglish = anime.getTitleEnglish();
+        this.types = anime.getTypes();
+        this.episodes = anime.getEpisodes();
+        this.status = anime.getStatus();
+        this.date = anime.getDate().toString();
+        this.rating = anime.getRating();
+        this.genres = String.join(",", anime.getGenres());
+        this.img = anime.getImg();
+    }
+
+    public AnimeDTO(UUID id, String title, String titleEnglish, String types, Integer episodes, String status, String date, String rating, String genres, String img) {
+        this.id = id;
+        this.title = title;
+        this.titleEnglish = titleEnglish;
+        this.types = types;
+        this.episodes = episodes;
+        this.status = status;
+        this.date = date;
+        this.rating = rating;
+        this.genres = genres;
+        this.img = img;
+    }
+
+    public Anime fromDTO(){
+        return new Anime(id,title,titleEnglish,types,episodes,status, Date.valueOf(date),rating, Arrays.stream(genres.split(",")).collect(Collectors.toList()),img);
+    }
+}
--- MyAnimeList/src/main/kotlin/com/example/myanimelist/repositories/admins/IAdminRepository.kt
+++ MyAnimeList/src/main/kotlin/com/example/myanimelist/repositories/admins/IAdminRepository.kt
@@ -2,7 +2,7 @@ package com.example.myanimelist.repositories.admins
 
 import com.example.myanimelist.models.Admin
 import com.example.myanimelist.repositories.ICRUDRepository
-import java.util.*
+import java.util.UUID
 
 interface IAdminRepository : ICRUDRepository<UUID, Admin?> {
 
