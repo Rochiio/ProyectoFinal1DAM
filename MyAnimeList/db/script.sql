658413c2730f289a3641718a85ddbe4a97fd7da0
Roberto Blázquez <elroby444@gmail.com>
2022-05-09 14:53:35 +0200

    Change .sql (#64)
    * Fix conflicts
    
    * Change .sql

--- MyAnimeList/db/script.sql
+++ MyAnimeList/db/script.sql
@@ -26,8 +26,7 @@ create table animes
     imageUrl      text        not null,
     episodes      integer     not null,
     rating        varchar(50) not null,
-    type          varchar(20),
-    scored_by     integer      default 0
+    type          varchar(20)
 );
 
 create table sqlite_master
@@ -47,13 +46,15 @@ create table sqlite_sequence
 
 create table usuarios
 (
-    id        varchar(50)  not null
+    id              varchar(50)  not null
         constraint usuarios_pk
             primary key,
-    nombre    varchar(100) not null,
-    date_alta Date         not null,
-    password  varchar(50)  not null,
-    imageurl  text
+    nombre          varchar(100) not null,
+    date_alta       Date         not null,
+    password        varchar(50)  not null,
+    imageurl        text,
+    email           varchar(100) not null,
+    date_nacimiento Date
 );
 
 create table animeLists
@@ -72,14 +73,12 @@ create table reviews
         references usuarios,
     idAnime varchar(50) not null
         references animes,
-    score   integer      default 0 not null,
+    score   integer,
     id      varchar(50)
         constraint reviews_pk
             primary key,
-    review  varchar(500) default NULL
+    review  varchar(500)
 );
 
 create unique index reviews_idAnime_uindex
     on reviews (idAnime);
-
-
