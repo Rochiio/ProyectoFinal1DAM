6c45a19634018512a92aa14d661bb96494f79c1c
Rocio <rpalaofernandez@gmail.com>
2022-05-09 13:52:53 +0200

    cambio reviews

--- MyAnimeList/src/main/kotlin/com/example/myanimelist/repositories/reviews/IRepositoryReview.kt
+++ MyAnimeList/src/main/kotlin/com/example/myanimelist/repositories/reviews/IRepositoryReview.kt
@@ -4,25 +4,6 @@ import com.example.myanimelist.models.Review
 import java.util.*
 
 
-interface IRepositoryReview {
-    /**
-     * Añadir reviews al repositorio
-     * @param review review a añadir
-     * @return la review añadida
-     */
-    fun add(review: Review): Review?
+interface IRepositoryReview: ICRUDReviews<Review,UUID> {
 
-    /**
-     * Mostrar todas las reviews de un anime
-     * @param animeId id del anime a ver todas sus reviews
-     * @return lista con todas las reviews de ese anime
-     */
-    fun findByAnimeId(animeId: UUID): List<Review>
-
-    /**
-     * Actualiza una review
-     * @param review review a actualizar
-     * @return review si ha sido actualizada
-     */
-    fun update(review: Review): Review?
 }
