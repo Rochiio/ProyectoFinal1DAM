564b5b099e65fe8893995a944d23323f53e36308
Bank <elroby444@gmail.com>
2022-05-09 14:14:15 +0200

    Fix conflicts

--- MyAnimeList/src/main/kotlin/com/example/myanimelist/models/Admin.kt
+++ MyAnimeList/src/main/kotlin/com/example/myanimelist/models/Admin.kt
@@ -4,11 +4,10 @@ import java.sql.Date
 import java.util.*
 
 data class Admin(
-    override val id: UUID = UUID.randomUUID(),
     override var name: String,
     override var email: String,
     override var password: String,
     override val createDate: Date,
     override val birthDate: Date,
-
+    override val id: UUID = UUID.randomUUID()
 ) : AbstractUser()
