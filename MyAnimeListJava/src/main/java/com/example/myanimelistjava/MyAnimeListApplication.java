e40e93af1b768ad7f97bfc5de59d16b631b98541
Rocio <rpalaofernandez@gmail.com>
2022-05-10 10:02:41 +0200

    checkDatabase añadido

--- MyAnimeListJava/src/main/java/com/example/myanimelistjava/MyAnimeListApplication.java
+++ MyAnimeListJava/src/main/java/com/example/myanimelistjava/MyAnimeListApplication.java
@@ -1,5 +1,6 @@
 package com.example.myanimelistjava;
 
+import com.example.myanimelistjava.managers.DataBaseManager;
 import com.example.myanimelistjava.managers.SceneManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,6 +8,9 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
 
 public class MyAnimeListApplication extends Application {
     @Override
@@ -20,6 +24,24 @@ public class MyAnimeListApplication extends Application {
     }
 
     public static void main(String[] args) {
+        checkDataBase();
         launch();
+    }
+
+    private static void checkDataBase() {
+        System.out.println("Comprobamos la conexión al Servidor BD");
+        DataBaseManager db = DataBaseManager.getInstance();
+        try {
+            db.open();
+            Optional<ResultSet> rs = db.select("SELECT 'Hello world'");
+            if (rs.isPresent()) {
+                rs.get().next();
+                db.close();
+                System.out.println("Conexión correcta a la Base de Datos");
+            }
+        } catch (SQLException e) {
+            System.err.println("Error al conectar al servidor de Base de Datos: " + e.getMessage());
+            System.exit(1);
+        }
     }
 }
