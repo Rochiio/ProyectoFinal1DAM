de8a7dd5aea03459919283054f81049809f1b1b6
JoaquinAyG <90848989+JoaquinAyG@users.noreply.github.com>
2022-05-10 09:13:45 +0200

    Optimize Interfaces

--- MyAnimeListJava/src/main/java/com/example/myanimelistjava/repositories/admin/IAdminRepository.java
+++ MyAnimeListJava/src/main/java/com/example/myanimelistjava/repositories/admin/IAdminRepository.java
@@ -2,8 +2,9 @@ package com.example.myanimelistjava.repositories.admin;
 
 import com.example.myanimelistjava.models.Admin;
 import com.example.myanimelistjava.models.User;
+import com.example.myanimelistjava.repositories.ICRUDRepository;
 
 import java.util.UUID;
 
-public interface IAdminRepository extends ICRUDAdmin<Admin, UUID> {
+public interface IAdminRepository extends ICRUDRepository<UUID, Admin> {
 }
