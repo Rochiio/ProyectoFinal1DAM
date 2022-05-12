7d98359cf2df7f7b00b04687c8ad163cf582e382
Kino <joaquin.ayllongar@gmail.com>
2022-05-12 18:20:50 +0200

    add some thing

--- MyAnimeList/src/main/kotlin/com/example/myanimelist/controllers/main/MainUserController.kt
+++ MyAnimeList/src/main/kotlin/com/example/myanimelist/controllers/main/MainUserController.kt
@@ -2,28 +2,52 @@ package com.example.myanimelist.controllers.main
 
 import com.example.myanimelist.models.Anime
 import com.example.myanimelist.models.User
+import com.example.myanimelist.repositories.animes.IAnimeRepository
+import com.example.myanimelist.repositories.users.IUsersRepository
+import com.example.myanimelist.service.anime.IAnimeStorage
+import com.example.myanimelist.service.img.IImgStorage
+import com.example.myanimelist.views.models.AnimeView
 import javafx.event.ActionEvent
 import javafx.fxml.FXML
-import javafx.scene.control.Button
-import javafx.scene.control.Label
-import javafx.scene.control.TableView
-import javafx.scene.control.TextField
+import javafx.scene.control.*
 import javafx.scene.input.KeyEvent
 import java.sql.SQLException
 import java.util.logging.LogManager
 import java.util.logging.Logger
 
 
-class MainUserController {
+class MainUserController(
+    var user : User,
+    var animeStorage: IAnimeStorage,
+    var imgStorage: IImgStorage,
+    var animeRepository: IAnimeRepository,
+    var usersRepository: IUsersRepository,
+    val logger : Logger = LogManager.getLogManager().getLogger("main_user.controller")
+) {
 
-
-    val logger : Logger = LogManager.getLogManager().getLogger("mainuser.controller")
-    lateinit var user : User;
 
     @FXML
-    private lateinit var myListTable: TableView<Anime>
+    private lateinit var animeRankingCol: TableColumn<AnimeView, String>
     @FXML
-    private lateinit var animeTable: TableView<Anime>
+    private lateinit var animeTitleCol: TableColumn<AnimeView, AnimeView>
+    @FXML
+    private lateinit var animeScoreCol: TableColumn<AnimeView, String>
+    @FXML
+    private lateinit var myListScoreCol: TableColumn<AnimeView, String>
+    @FXML
+    private lateinit var myListTypeCol: TableColumn<AnimeView, String>
+    @FXML
+    private lateinit var myListStatusCol: TableColumn<AnimeView, String>
+    @FXML
+    private lateinit var myListGenderCol: TableColumn<AnimeView, String>
+    @FXML
+    private lateinit var myListTitleCol: TableColumn<AnimeView, AnimeView>
+    @FXML
+    private lateinit var myListRankingCol: TableColumn<AnimeView, String>
+    @FXML
+    private lateinit var myListTable: TableView<AnimeView>
+    @FXML
+    private lateinit var animeTable: TableView<AnimeView>
     @FXML
     private lateinit var myListNameSearch: TextField
     @FXML
@@ -41,6 +65,30 @@ class MainUserController {
     @FXML
     private lateinit var generateButton: Button
 
+    @FXML
+    private fun initialize() {
+
+        // DaggerRepositoryFactory.create().inject(this);
+
+        try {
+            loadData()
+        } catch (e: SQLException) {
+            logger.warning("error while loading")
+        }
+
+        // Asigno las columnas de la tabla
+        animeScoreCol.setCellValueFactory { cellData -> cellData.value.imgProperty() }
+
+        // Asigno los manejadores de eventos
+        personasTable.getSelectionModel().selectedItemProperty().addListener { observable, oldValue, newValue ->
+            onPersonaSeleccionada(
+                newValue
+            )
+        }
+        clearDataInfo()
+        personasTable.getSelectionModel().selectFirst()
+    }
+
     fun generateHTML(actionEvent: ActionEvent) {
 
     }
@@ -54,7 +102,7 @@ class MainUserController {
     }
 
     private fun loadData() {
-        logger.info("Cargando datos...")
+        logger.info("loading files")
 
     }
 }
