e720fefdd73dbc91501db290105e71fec188c3d0
Mohamed Asidah Bchiri <37219066+loopedmoha@users.noreply.github.com>
2022-05-17 08:45:56 +0200

    Develop (#114)
    * eliminado admin de BackupStorageTest.kt
    
    * eliminado admin de BackupStorageTest.kt
    
    * He cambiado las inicializaciones de BackupStorageTest.kt para que funcione con como lo tenemos ahora

--- MyAnimeList/src/test/kotlin/com/example/myanimelist/services/BackupStorageTest.kt
+++ MyAnimeList/src/test/kotlin/com/example/myanimelist/services/BackupStorageTest.kt
@@ -1,29 +1,34 @@
 package com.example.myanimelist.services
 
 import com.example.myanimelist.dto.BackupDTO
+import com.example.myanimelist.manager.DataBaseManager
+import com.example.myanimelist.managers.DependenciesManager
 //import com.example.myanimelist.modules.repositoryModule
 import com.example.myanimelist.repositories.admins.IAdminRepository
+import com.example.myanimelist.repositories.animes.AnimeRepository
 import com.example.myanimelist.repositories.animes.IAnimeRepository
 import com.example.myanimelist.repositories.reviews.IRepositoryReview
+import com.example.myanimelist.repositories.reviews.ReviewsRepository
 import com.example.myanimelist.repositories.users.IUsersRepository
+import com.example.myanimelist.repositories.users.UsersRepository
+import com.example.myanimelist.service.backup.BackupStorage
 import com.example.myanimelist.service.backup.IBackupStorage
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
 
 
 class BackupStorageTest  {
-    private lateinit var storage: IBackupStorage
-    private lateinit var animesRepo: IAnimeRepository
-    private lateinit var usersRepo: IUsersRepository
-    private lateinit var reviewRepo: IRepositoryReview
-    private lateinit var adminRepo: IAdminRepository
+    private   var storage: IBackupStorage  = BackupStorage()
+    private  var animesRepo: IAnimeRepository = DependenciesManager.getAnimesRepo()
+    private  var usersRepo: IUsersRepository = DependenciesManager.getUsersRepo()
+    private  var reviewRepo: IRepositoryReview = DependenciesManager.getReviewsRepo()
 
 
 
     @Test
     @Order(0)
     fun save() {
-        val dto = BackupDTO(usersRepo.findAll(), reviewRepo.findAll(), animesRepo.findAll(), adminRepo.findAll())
+        val dto = BackupDTO(usersRepo.findAll(), reviewRepo.findAll(), animesRepo.findAll())
         storage.save(dto)
     }
 
