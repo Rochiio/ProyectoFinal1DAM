ad659e4e7824ac7a655b3f3d732d1dbc271a3d71
Bank <elroby444@gmail.com>
2022-05-17 10:26:17 +0200

    Filters improved

--- MyAnimeList/src/main/kotlin/com/example/myanimelist/filters/login/LoginFilters.kt
+++ MyAnimeList/src/main/kotlin/com/example/myanimelist/filters/login/LoginFilters.kt
@@ -3,6 +3,8 @@ package com.example.myanimelist.filters.login
 import com.example.myanimelist.repositories.users.IUsersRepository
 
 open class LoginFilters(protected val usersRepository: IUsersRepository) {
-    fun checkUserCorrect(name: String, pass: String) = usersRepository.findByName(name).any { it.password == pass }
+    fun checkUserCorrect(name: String, pass: String) = usersRepository.findByName(name).any {
+        it.name == name && it.password == pass
+    }
 }
 
