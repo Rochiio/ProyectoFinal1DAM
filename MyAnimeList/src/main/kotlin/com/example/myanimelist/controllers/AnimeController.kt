423052ca18803936e3d0c1f1f104364f692b7c16
Rocio <rpalaofernandez@gmail.com>
2022-05-18 10:52:55 +0200

    creado anime controller

--- MyAnimeList/src/main/kotlin/com/example/myanimelist/controllers/AnimeController.kt
+++ MyAnimeList/src/main/kotlin/com/example/myanimelist/controllers/AnimeController.kt
@@ -1,5 +1,82 @@
 package com.example.myanimelist.controllers
 
+import com.example.myanimelist.extensions.loadScene
+import com.example.myanimelist.extensions.show
+import com.example.myanimelist.managers.DependenciesManager
+import com.example.myanimelist.repositories.animes.IAnimeRepository
+import com.example.myanimelist.utils.ANIME_DATA
+import com.example.myanimelist.utils.HEIGHT
+import com.example.myanimelist.utils.WIDTH
+import com.example.myanimelist.views.models.AnimeView
+import javafx.event.ActionEvent
+import javafx.fxml.FXML
+import javafx.scene.control.Alert
+import javafx.scene.control.Button
+import javafx.scene.control.ButtonType
+import javafx.scene.control.Label
+import javafx.stage.Stage
+
+
 class AnimeController {
 
+    @FXML
+     lateinit var txtTittle: Label
+    @FXML
+     lateinit var txtEpisodes: Label
+    @FXML
+     lateinit var txtStatus: Label
+    @FXML
+     lateinit var txtDate: Label
+    @FXML
+     lateinit var txtGenre: Label
+    @FXML
+     lateinit var btnAdd: Button
+
+
+    private lateinit var anime: AnimeView
+    private var animeRepository: IAnimeRepository = DependenciesManager.getAnimesRepo()
+    //TODO añadir repositorio animeLists para añadir animes del usuario loggeado
+
+
+
+    fun addToList(actionEvent: ActionEvent) {
+        val alert = Alert(Alert.AlertType.CONFIRMATION)
+        alert.title = "Confirmación"
+        alert.contentText = "Quiere añadir el anime ${txtTittle.text} a su lista"
+        val action = alert.showAndWait()
+
+            if (action.get() == ButtonType.OK) {
+                var user = DependenciesManager.globalUser
+                var animeAux= animeRepository.findById(anime.id)
+
+                //TODO añadir anime id y user id al repo de animelist
+
+            } else {
+                val alert= Alert(Alert.AlertType.INFORMATION)
+                alert.title="Saliendo del anime"
+                val stage = txtTittle.scene.window as Stage
+                stage.close()
+            }
+
+    }
+
+    fun initStageAnimeData(anime: AnimeView){
+        this.anime=anime
+        val stage = Stage()
+            stage.loadScene(ANIME_DATA, WIDTH, HEIGHT) {
+                title = "Anime-Data"
+                isResizable = false
+            }.show()
+
+            showAnime()
+    }
+
+    private fun showAnime(){
+        txtTittle.text=anime.presentation.title
+        txtEpisodes.text=anime.episodes.toString()
+        txtStatus.text=anime.status.toString()
+        txtDate.text=anime.date.toString()
+        txtGenre.text=anime.genres.toString()
+
+    }
 }
--- MyAnimeList/src/main/kotlin/com/example/myanimelist/controllers/main/MainUserController.kt
+++ MyAnimeList/src/main/kotlin/com/example/myanimelist/controllers/main/MainUserController.kt
@@ -1,7 +1,9 @@
 package com.example.myanimelist.controllers.main
 
+import com.example.myanimelist.controllers.AnimeController
 import com.example.myanimelist.factories.TableItemFactory
 import com.example.myanimelist.managers.DependenciesManager
+import com.example.myanimelist.managers.DependenciesManager.getAnimeController
 import com.example.myanimelist.managers.DependenciesManager.getAnimesRepo
 import com.example.myanimelist.managers.DependenciesManager.getLogger
 import com.example.myanimelist.managers.DependenciesManager.getUsersRepo
@@ -38,6 +40,7 @@ class MainUserController(
     lateinit var imgStorage: IImgStorage
     private var animeRepository: IAnimeRepository = getAnimesRepo()
     private var usersRepository: IUsersRepository = getUsersRepo()
+    private var animeController: AnimeController = getAnimeController()
     var itemFactory: TableItemFactory = TableItemFactory()
 
     private var animeList: ObservableList<AnimeView> = FXCollections.observableArrayList()
@@ -222,7 +225,8 @@ class MainUserController(
     fun addToMyList(mouseEvent: MouseEvent) {
         if (mouseEvent.button === MouseButton.PRIMARY && mouseEvent.clickCount == 2) {
             val anime: AnimeView = animeTable.selectionModel.selectedItem
-            addAnime(anime)
+            animeController.initStageAnimeData(anime)
+            //addAnime(anime)
         }
     }
 
