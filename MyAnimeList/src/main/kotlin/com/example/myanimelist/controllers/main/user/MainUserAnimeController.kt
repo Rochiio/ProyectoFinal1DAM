18891e78221b66ba755494f1c5827ab57ac28938
Kino <joaquin.ayllongar@gmail.com>
2022-05-22 12:22:43 +0200

    cambio a list view test

--- MyAnimeList/src/main/kotlin/com/example/myanimelist/controllers/main/user/MainUserAnimeController.kt
+++ MyAnimeList/src/main/kotlin/com/example/myanimelist/controllers/main/user/MainUserAnimeController.kt
@@ -3,48 +3,36 @@ package com.example.myanimelist.controllers.main.user
 import com.example.myanimelist.extensions.loadScene
 import com.example.myanimelist.managers.DependenciesManager
 import com.example.myanimelist.repositories.animes.IAnimeRepository
+import com.example.myanimelist.service.img.IImgStorage
 import com.example.myanimelist.utils.*
 import com.example.myanimelist.views.models.AnimeView
-import com.example.myanimelist.views.models.Presentation
-import com.example.myanimelist.views.tableextensions.PresentationCellFactory
 import javafx.collections.FXCollections
 import javafx.collections.ObservableList
-import javafx.collections.transformation.FilteredList
 import javafx.fxml.FXML
 import javafx.geometry.Pos
-import javafx.scene.control.Button
-import javafx.scene.control.TableCell
-import javafx.scene.control.TableColumn
-import javafx.scene.control.TableView
-import javafx.scene.control.cell.PropertyValueFactory
+import javafx.scene.control.*
 import javafx.scene.image.Image
 import javafx.scene.image.ImageView
 import javafx.scene.input.MouseButton
 import javafx.scene.input.MouseEvent
+import javafx.scene.layout.HBox
+import javafx.scene.layout.VBox
 import javafx.stage.Stage
 import org.apache.logging.log4j.Logger
 
 class MainUserAnimeController {
 
+
+
     //Generics
     val logger: Logger = DependenciesManager.getLogger<MainUserAnimeController>()
     val user = DependenciesManager.globalUser
+    val imgStorage: IImgStorage = DependenciesManager.getImgStorage()
 
     //FXML
     @FXML
-    private lateinit var animeButtonCol: TableColumn<AnimeView, Button>
+    private lateinit var animeListView: ListView<AnimeView>
 
-    @FXML
-    private lateinit var animeRatingCol: TableColumn<AnimeView, String>
-
-    @FXML
-    private lateinit var animeTitleCol: TableColumn<AnimeView, Presentation>
-
-    @FXML
-    private lateinit var animeRankingCol: TableColumn<AnimeView, Int>
-
-    @FXML
-    private var animeTable: TableView<AnimeView> = TableView()
 
     //Specific
     private var animeList: ObservableList<AnimeView> = FXCollections.observableArrayList()
@@ -67,41 +55,72 @@ class MainUserAnimeController {
     }
 
     private fun initCells() {
-        animeRankingCol.setCellValueFactory { cellData -> cellData.value.rankingProperty().asObject() }
-        animeTitleCol.setCellValueFactory { cellData -> cellData.value.presentationProperty() }
-        animeTitleCol.setCellFactory { PresentationCellFactory() }
-        animeRatingCol.setCellValueFactory { cellData -> cellData.value.ratingProperty() }
-        animeButtonCol.setCellFactory {
-            object : TableCell<AnimeView, Button>() {
-                override fun updateItem(item: Button?, empty: Boolean) {
-                    val but = Button()
-                    but.alignment = Pos.CENTER
-                    val img = ImageView()
-                    img.image = Image(Properties.ADD_ICON)
-                    img.fitHeight = but.height
-                    img.fitWidth = but.width
-                    but.graphic = img
-                    but.setOnAction {
-                        val anime: AnimeView = animeTable.selectionModel.selectedItem
-                        DependenciesManager.animeSelection = anime
-                        Stage().loadScene(ANIME_DATA, WIDTH, HEIGHT) {
-                            title = anime.presentation.title
-                            isResizable = false
-                        }.show()
-                    }
-                    graphic = but
+        animeListView.setCellFactory {object:ListCell<AnimeView>(){
+            override fun updateItem(item: AnimeView?, empty: Boolean) {
+                super.updateItem(item, empty)
+                if (empty) {
+                    text = null
+                    graphic = null
+                    return
+                }
+
+                //Iniciamos la base
+                val root = HBox()
+                root.spacing = 30.0
+
+                //Iniciamos los componentes
+
+                //Ranking
+                val ranking = Label(item!!.ranking.toString())
+
+                //Presentacion (Imagen + titulos)
+                val presentationRoot = VBox()
+                presentationRoot.spacing = 20.0
+                presentationRoot.alignment = Pos.CENTER_LEFT
+                val hbox = HBox()
+                hbox.spacing = 10.0
+                hbox.children.add(Label(item.presentation.title))
+                hbox.children.add(Label(item.presentation.titleEnglish))
+                val imageview = ImageView()
+                imageview.fitHeight = 40.0
+                imageview.fitWidth = 40.0
+                imageview.image = imgStorage.loadImg(item.presentation)
+                presentationRoot.children.add(imageview)
+                presentationRoot.children.add(hbox)
+
+                //Rating
+                val rating = Label(item.rating)
+
+                //Boton De Añadir
+                val but = Button()
+                but.alignment = Pos.CENTER
+                val img = ImageView()
+                img.image = Image(Properties.ADD_ICON)
+                img.fitHeight = but.height
+                img.fitWidth = but.width
+                but.graphic = img
+                but.setOnAction {
+                    DependenciesManager.animeSelection = item
+                    Stage().loadScene(ANIME_DATA, WIDTH, HEIGHT) {
+                        title = item.presentation.title
+                        isResizable = false
+                    }.show()
+                }
+                //Hacemos el setGraphic
+
+                root.children.addAll(ranking, presentationRoot, rating, but)
+
+                graphic = root
                 }
             }
         }
-        animeTable.items = animeList
-        animeTable.columns.addAll(animeRankingCol, animeTitleCol, animeRatingCol, animeButtonCol)
     }
 
     fun changeSceneToAnimeDataView(mouseEvent: MouseEvent) {
         if (mouseEvent.button === MouseButton.PRIMARY && mouseEvent.clickCount == 2) {
-            val anime: AnimeView = animeTable.selectionModel.selectedItem
+            val anime: AnimeView = animeListView.selectionModel.selectedItem
             DependenciesManager.animeSelection = anime
-            Stage().loadScene(ANIME_DATA, WIDTH, HEIGHT) {
+            Stage().loadScene(ANIME_DATA, WIDTH, HEIGHT){
                 title = anime.presentation.title
                 isResizable = false
             }.show()
