f06881cb1dd5c6f591af1518408461ea24195958
Rocio <rpalaofernandez@gmail.com>
2022-05-22 16:31:23 +0200

    ya se ven las imagenes

--- MyAnimeList/src/main/kotlin/com/example/myanimelist/managers/ResourcesManager.kt
+++ MyAnimeList/src/main/kotlin/com/example/myanimelist/managers/ResourcesManager.kt
@@ -5,7 +5,7 @@ import java.io.InputStream
 
 object ResourcesManager {
 
-    fun getCoverOf(value: String): InputStream? = MyAnimeListApplication::class.java.getResourceAsStream("covers/$value")
+    fun getCoverOf(value: String): InputStream? = MyAnimeListApplication::class.java.getResourceAsStream("covers/$value.jpg")
     fun getIconOf(value: String): InputStream? = MyAnimeListApplication::class.java.getResourceAsStream("icons/$value")
     fun getStyleOf(value: String): InputStream? = MyAnimeListApplication::class.java.getResourceAsStream("styles/$value")
     fun getViewsOf(value: String): InputStream? = MyAnimeListApplication::class.java.getResourceAsStream("views/$value")
