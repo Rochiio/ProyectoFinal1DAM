3753e9bebfb20be3a5dcf052ebe4e414dbb707ce
Kino <joaquin.ayllongar@gmail.com>
2022-05-27 21:02:10 +0200

    anime add view

---
+++ MyAnimeList/src/main/kotlin/com/example/myanimelist/controllers/anime/AnimeAddController.kt
@@ -0,0 +1,25 @@
+package com.example.myanimelist.controllers.anime
+
+import javafx.scene.control.ChoiceBox
+import javafx.scene.control.DatePicker
+import javafx.scene.control.Label
+import javafx.scene.image.ImageView
+
+class AnimeAddController {
+    lateinit var txtTitleEnglish: Label
+    lateinit var txtTittle: Label
+    lateinit var txtEpisodes: Label
+    lateinit var datePicker: DatePicker
+    lateinit var statusCB: ChoiceBox<Any>
+    lateinit var typeCB: ChoiceBox<Any>
+    lateinit var GenreCB: ChoiceBox<Any>
+    lateinit var imageAnime: ImageView
+
+    fun changeAnimeImg() {
+
+    }
+
+    fun onSave() {
+
+    }
+}
